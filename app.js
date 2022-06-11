6fc2b2a45da4ce2a5921d7282b55e4463ad350f7
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-06-11 13:39:28 +0600

    redis cache use

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ const express = require('express');
 const axios = require('axios');
 const redis = require('redis');
 const responseTime = require('response-time');
+const {promisify} = require('util');
 
 const app = express();
 app.use(responseTime());
 
+const client = redis.createClient({
+    host: '127.0.0.1',
+    port: '6379'
+})
+
+const GET_ASYNC = promisify(client.get).bind(client);
+const SET_ASYNC = promisify(client.set).bind(client);
+
 app.get('/rockets', async(req, res, next)=>{
     try {
+
+        const reply = await GET_ASYNC('rockets');
+        if(reply){
+            console.log('using cached data');
+            res.send(JSON.parse(reply))
+            return
+        }
+
         const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+        const saveResult = await SET_ASYNC('rockets', JSON.stringify(response.data), 'EX', 5);
+
+        console.log('New Data cashed', saveResult);
         res.send(response.data);
+        
     } catch (error) {
         res.send(error.message);
     }
